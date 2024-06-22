12d1f4004fabbcd6b29e9626b1dee47a9769f5a8
Kocak Osman <73236836+OK1798@users.noreply.github.com>
2024-06-22 21:27:17 +0200

    Update PassengerArrivalEvent.java

--- src/airport/events/PassengerArrivalEvent.java
+++ src/airport/events/PassengerArrivalEvent.java
@@ -1,6 +1,8 @@
 package airport.events;
 
 import airport.AirportCarRentalModel;
+import airport.IdManager;
+import airport.entities.StationEntity;
 import co.paralleluniverse.fibers.SuspendExecution;
 import desmoj.core.simulator.Event;
 import desmoj.core.simulator.Model;
@@ -8,6 +10,7 @@ import airport.entities.PassengerEntity;
 
 public class PassengerArrivalEvent extends Event<PassengerEntity> {
     private final AirportCarRentalModel meinModel;
+
     public PassengerArrivalEvent(Model model, String name, boolean showInTrace) {
         super(model, name, showInTrace);
         meinModel = (AirportCarRentalModel) model;
@@ -15,6 +18,11 @@ public class PassengerArrivalEvent extends Event<PassengerEntity> {
 
     @Override
     public void eventRoutine(PassengerEntity passengerEntity) throws SuspendExecution {
+        int stationId = passengerEntity.getDestinationId();
+        StationEntity station = IdManager.getStation(stationId);
+        meinModel.sendTraceNote("Passenger " + passengerEntity.getName() + " arrives at " + station.getName());
 
+        // Angekommene Passagier in die Station Queue hinzufügen
+        station.getQueue().add(passengerEntity);
     }
 }

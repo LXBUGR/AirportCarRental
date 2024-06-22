7f67ba2998b7f98130f96ed01a1e3352702ccec8
Kocak Osman <73236836+OK1798@users.noreply.github.com>
2024-06-22 21:25:45 +0200

    Update BusLeaveEvent.java

--- src/airport/events/BusLeaveEvent.java
+++ src/airport/events/BusLeaveEvent.java
@@ -1,13 +1,17 @@
 package airport.events;
 
+import airport.IdManager;
 import co.paralleluniverse.fibers.SuspendExecution;
 import desmoj.core.simulator.Event;
 import desmoj.core.simulator.Model;
+import desmoj.core.simulator.TimeInstant;
 import airport.entities.StationEntity;
+import airport.entities.BusEntity;
 import airport.AirportCarRentalModel;
 
 public class BusLeaveEvent extends Event<StationEntity> {
     private final AirportCarRentalModel meinModel;
+
     public BusLeaveEvent(Model model, String name, boolean showInTrace) {
         super(model, name, showInTrace);
         meinModel = (AirportCarRentalModel) model;
@@ -15,6 +19,18 @@ public class BusLeaveEvent extends Event<StationEntity> {
 
     @Override
     public void eventRoutine(StationEntity stationEntity) throws SuspendExecution {
+        BusEntity bus = meinModel.getBus();
+        double nextStationId = bus.getNextStationId();
 
+        // Ankunft des Buses am nächsten Station planen
+        StationEntity nextStation = IdManager.getStation((int) nextStationId);
+        double travelTime = bus.getNextStationDriveTime();
+
+        // Neue ereignis für die Busankunft an der nächsten Station
+        BusArrivalEvent arrivalEvent = new BusArrivalEvent(meinModel, "Bus Arrival Event", true);
+        arrivalEvent.schedule(nextStation, new TimeInstant(travelTime));
+
+        // Bus abfahrt loggen
+        meinModel.sendTraceNote("Bus departs from " + stationEntity.getName() + " to " + nextStation.getName());
     }
 }

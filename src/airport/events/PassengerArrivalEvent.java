fa4899b10c33279fb69d7ac9b66eeaf757e79cf2
Kocak Osman <73236836+OK1798@users.noreply.github.com>
2024-06-24 12:21:08 +0200

    Update PassengerArrivalEvent.java

--- src/airport/events/PassengerArrivalEvent.java
+++ src/airport/events/PassengerArrivalEvent.java
@@ -3,10 +3,10 @@ package airport.events;
 import airport.AirportCarRentalModel;
 import airport.IdManager;
 import airport.entities.BusEntity;
+import airport.entities.PassengerEntity;
 import airport.entities.StationEntity;
 import desmoj.core.simulator.Event;
 import desmoj.core.simulator.Model;
-import airport.entities.PassengerEntity;
 
 public class PassengerArrivalEvent extends Event<PassengerEntity> {
     private final AirportCarRentalModel meinModel;
@@ -22,14 +22,12 @@ public class PassengerArrivalEvent extends Event<PassengerEntity> {
         meinModel.sendTraceNote("Passenger " + passengerEntity.getName() + " arrives at " + station.getName());
 
         BusEntity bus = meinModel.getBus();
-        if(!bus.isDriving() && bus.getCurrentStationId() == passengerEntity.getArrivalId() && bus.getPassengerCount() < bus.getCapacity()) {
-            //Angekommenen Passagier steigt in Bus ein
+        if (!bus.isDriving() && bus.getCurrentStationId() == passengerEntity.getArrivalId() && bus.getPassengerCount() < bus.getCapacity()) {
             bus.addPassenger(passengerEntity);
-            if(bus.getPassengerCount() == bus.getCapacity()) {
-               meinModel.currentBusLeave.reSchedule(meinModel.presentTime());
+            if (bus.getPassengerCount() == bus.getCapacity()) {
+                meinModel.currentBusLeave.reSchedule(meinModel.presentTime());
             }
         } else {
-            // Angekommene Passagier in die Station Queue hinzufügen
             station.enqueuePassenger(passengerEntity);
         }
     }
